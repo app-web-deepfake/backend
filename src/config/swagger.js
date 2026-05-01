5b07a5970d86bc40f4d44a9353e7c15563be1b13
Armix18 <armix182002@gmail.com>
2026-05-01 17:03:19 -0500

    feat: add swagger url

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const swaggerSpec = yaml.load(path.join(__dirname, "../docs/openapi.yaml"));
-
+const swaggerSpec = yaml.load(
+    path.resolve(__dirname, "../docs/openapi.yaml")
+);
 export default swaggerSpec;
